71cf499a8fc73216fd2052d40c02bb1ba1cf1559
YYJ <paopao@YongjiaotekiMacBook-Air.local>
2014-06-19 01:30:10 -0400

    .h file for class simplifyPath

---
+++ simplifyPath.h
@@ -0,0 +1,59 @@
+//
+//  simplifyPath.h
+//  RDP
+//
+//  Created by Yongjiao Yu on 14-6-13.
+//  Copyright (c) 2014年 ___RCPD___. All rights reserved.
+//
+
+
+#ifndef __HELLO__H__
+#define __HELLO__H__
+
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <cmath>
+
+//"Point" struct stand for each GPS coordinates(x,y). Methods related are used only for simplification of calculation in implementation.
+
+typedef struct Point{
+    double x;
+    double y;
+    
+    double operator*(Point rhs)const{
+        return (this->x * rhs.y - rhs.x * this->y);
+    }
+    
+    Point operator-(Point rhs)const{
+        Point p;
+        p.x=this->x-rhs.x;
+        p.y=this->y-rhs.y;
+        return p;
+    }
+    
+//    Point& operator-(Point rhs){
+//        this->x=this->x-rhs.x;
+//        this->y=this->y-rhs.y;
+//        return *this;
+//    }
+    
+    double Norm()const{
+        return sqrt(this->x * this->x + this->y * this->y);
+    }
+    
+}Point;
+
+class simplifyPath{
+//"findMaximumDistance" used as part of implementation for RDP algorithm.
+private:
+    const std::pair<int, double> findMaximumDistance(const std::vector<Point>& Points)const;
+    
+//"simplifyWithRDP" returns the simplified path with a Point vector. The function takes in the paths to be simplified and a customerized thresholds for the simplication.
+public:
+    std::vector<Point> simplifyWithRDP(std::vector<Point>& Points, double epsilon)const;
+};
+
+#endif 
+
+
