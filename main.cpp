1a61f711abff175134f9150c15ccd1e3834019d7
YYJ <paopao@YongjiaotekiMacBook-Air.local>
2014-06-19 01:42:03 -0400

    hello.cpp removed.

--- main.cpp
+++ main.cpp
@@ -2,17 +2,31 @@
 //  main.cpp
 //  RDP
 //
-//  Created by Yongjiao Yu on 14-6-13.
+//  Created by Yongjiao Yu on 14-6-17.
 //  Copyright (c) 2014年 ___RCPD___. All rights reserved.
 //
 
-//#include <iostream>
-//
-//int main(int argc, const char * argv[])
-//{
-//
-//    // insert code here...
-//    std::cout << "Hello, World!\n";
-//    return 0;
-//}
-//
+#include "simplifyPath.h"
+using std::vector;
+
+int main(){
+    vector<Point> Points(5);
+    Points[0].x=0;
+    Points[0].y=0;
+    Points[1].x=5;
+    Points[1].y=9;
+    Points[2].x=10;
+    Points[2].y=10;
+    Points[3].x=15;
+    Points[3].y=7;
+    Points[4].x=20;
+    Points[4].y=0;
+    double epsilon=2;
+    
+    simplifyPath testobject;
+    vector<Point> path=testobject.simplifyWithRDP(Points,epsilon);
+    for(vector<Point>::const_iterator i = path.begin(); i < path.end(); ++i)
+        std::cout <<'('<<i->x<< ", "<<i->y << ") ";
+    std::cout<<std::endl;
+    return EXIT_SUCCESS;
+}
